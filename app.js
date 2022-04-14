a2c1ab2d5742d43efeb5fcae8eca9c4da53b9e62
gary <gary.white@lodgit.com>
2022-04-14 12:51:10 +0200

    code

---
+++ app.js
@@ -0,0 +1,14 @@
+function correctTail(bod, tail ;
+
+    sub = body.substr(bodylength - (tail.length)
+    
+    if sub = tail);
+return true
+    }
+    else
+return false
+
+
+console.log(correctTail("Fox", "x")); // true
+console.log(correctTail("Rhino", "o")); // true
+console.log(correctTail("Meerkat", "t")); // true
